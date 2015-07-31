04bd90947572784b35e2925312f25955ddb42872
Shannon Graham <shannongraham@Shannons-MacBook-Air.local>
2015-07-30 21:30:12 -0400

    minor changes to vizualizer

---
+++ VizualizeBlockClosure_bjm.m
@@ -0,0 +1,15 @@
+function VizualizeBlockClosure_bjm(segmentFile,blockFile,stationFile)
+% blocks preprocessing tool to vizualize the block closing and segment
+% marching
+% provide segment file, block file and station file in that order
+
+fprintf('Parsing segment, block, and station files...')
+Segment = ReadSegmentStructUtil(segmentFile); % read segment file
+Block = ReadBlock(blockFile); % Read block file
+Station = ReadStation(stationFile); %read station file
+fprintf('done.\n')
+
+% edit this section
+fprintf('Labeling blocks...')
+[Segment, Block, Station] = BlockLabelViz_bjm(Segment, Block, Station);
+fprintf('done.\n')
